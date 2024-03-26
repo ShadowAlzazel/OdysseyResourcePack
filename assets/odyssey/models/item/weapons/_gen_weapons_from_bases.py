0ecb1bf3a653455186e49971658c2f302fa6bade
ShadowAlzazel <pastenalfredojr@gmail.com>
2024-03-26 15:08:15 -0700

    Succesful Script creator for tools

--- assets/odyssey/models/item/weapons/_gen_weapons_from_bases.py
+++ assets/odyssey/models/item/weapons/_gen_weapons_from_bases.py
@@ -9,37 +9,39 @@ os.chdir(dir_name)
 # Add [Polexe], [Battleaxe(labrys)]
 # Rework [Rapier], [Lance]
 
-weapon_types = ['charkam', 'claymore', 'cutlass', 'dagger', 'halberd', 'katana', 'kunai', 'lance', 
+tool_types = ['chakram', 'claymore', 'cutlass', 'dagger', 'halberd', 'katana', 'kunai', 'lance', 
                 'longaxe', 'longsword', 'rapier', 'saber', 'scythe', 'sickle', 'spear', 'warhammer']
-materials = ['wooden', 'golden', 'stone', 'iron', 'diamond', 'netherite'
+materials = ['wooden', 'golden', 'stone', 'iron', 'diamond', 'netherite',
              'copper', 'mithril', 'soul_steel']
 
-# Check 
-print("Confirm Creation of New files? This will overwrite old files (y/n) . . .")
-answer = inputs()
-
-# Create files
-if answer == "y":
-    pass 
-
-
-def create_files():
+# Function to create files
+def create_tool_files():
     for i in range(len(materials)):
-        for j in range(len(weapon_types)):
+        for j in range(len(tool_types)):
             mat = materials[i]
-            wtyp = weapon_types[j]
+            ttyp = tool_types[j]
             # Create json obj to make interpolation easier
-            filename = f'{mat}_{wtyp}.json'
+            filename = f'{mat}_{ttyp}.json'
             json_obj = {
-                "parent": f"odyssey:item/weapons/bases/{wtyp}",
+                "parent": f"odyssey:item/weapons/bases/{ttyp}",
                 "textures": {
-                    "0": f"odyssey:item/weapons/{wtyp}_texture_{mat}",
-                    "particle": f"odyssey:item/weapons/{wtyp}_texture_{mat}"
+                    "0": f"odyssey:item/weapons/{ttyp}_texture_{mat}",
+                    "particle": f"odyssey:item/weapons/{ttyp}_texture_{mat}"
                 }
             }
-            #text = f'{"parent": "odyssey:item/weapons/bases/{wtyp}", "textures": {"0": "odyssey:item/weapons/longaxe_texture_diamond", "particle": "odyssey:item/weapons/longaxe_texture_diamond"}}'
+            #text = f'{"parent": "odyssey:item/weapons/bases/{ttyp}", "textures": {"0": "odyssey:item/weapons/longaxe_texture_diamond", "particle": "odyssey:item/weapons/longaxe_texture_diamond"}}'
             text = json.dumps(json_obj)
             # Write the text to opened file
             with open(filename, 'w') as file:
                 file.write(text)
             
+# Prompt 
+print("Confirm Creation of New files? This will overwrite old files.")
+print(f"Will Create {len(tool_types) * len(materials)} tool combinations.")
+print("Proceed (y/n) . . .")
+answer = input()
+
+# Input
+if answer == "y":
+    print("Ok")
+    create_tool_files() 
